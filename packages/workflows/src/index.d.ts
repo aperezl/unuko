3299d357d28d996e86652ee62e463caa03eabc62
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2026-05-14 07:58:59 +0200

    clean

--- packages/workflows/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './sgp22/provisioning.machine';
-export * from './sgp22/inventory.machine';
-export * from './sgp22/profile-mgmt.machine';
-export * from './sgp22/notification.machine';
-export * from './sgp22/test.machine';
-export * from './sgp22/types';
-export * from './base/engine';
